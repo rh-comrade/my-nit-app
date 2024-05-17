a2791f3a359326dbde869176b9715edfb97a73dd
Rhutik Chaudhari <rhutikchaudhari662@gmail.com>
2024-05-17 17:52:02 +0530

    us001: home and navbar updated

--- src/header/header.jsx
+++ src/header/header.jsx
@@ -1,17 +1,10 @@
 import React from 'react'
-import Link from 'next/link'
-import navItem from './config/config.json';
-import styles from './header.module.css';
-
+import styles from './header.module.css'
 const Header = () => {
   return (
-    <nav className={styles.nav}>
-        {
-        navItem?.map(({item,route},index)=>{
-            return <Link  className={styles.link} key={`nav_${index}`}  href={route}>{item}</Link>
-        })
-      }
-    </nav>
+    <h2 className={styles.header}>
+        My NIT App
+    </h2>
   )
 }
 
